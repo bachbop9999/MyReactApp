adb2849207c3fc9d67800807601cfd82fd857c09
bachbop9999 <bachbop9999@gmail.com>
2023-05-26 16:16:39 +0700

    fix dockerfile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,9 @@
 import express from 'express'
-import { signin, signup, forgotPassword, resetPassword } from '../controllers/user.js'
+import { signin, signup } from '../controllers/user.js'
 
 const router = express.Router()
 
 router.post('/signin', signin)
 router.post('/signup', signup)
-router.post('/forgot', forgotPassword);
-router.post('/reset', resetPassword);
 
 export default router
